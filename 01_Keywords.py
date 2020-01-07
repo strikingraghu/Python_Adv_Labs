90e482d6d5c0c2177c8414da998228984a162021
Raghavendra Guttur <strikingraghu@gmail.com>
2020-01-07 09:29:28 +0530

    Python

--- 01_Keywords.py
+++ 01_Keywords.py
@@ -1,6 +1,7 @@
 """Code to provide details about Keywords being used in Python language"""
 import keyword
 
+# Scenario 1
 # Initializing strings for testing the code
 sample_1 = "Bangalore"
 sample_2 = "else"
@@ -8,6 +9,7 @@ sample_3 = "elif"
 sample_4 = "true"
 
 print()
+print("# Scenario 1")
 if keyword.iskeyword(sample_1):
     print("Sample 1 is having keyword reference value")
 else:
@@ -28,5 +30,68 @@ if keyword.iskeyword(sample_4):
 else:
     print("Sample 4 = ", sample_4)
 
+# Scenario 2
+# Explaining True, False, None, and, or, not within Python code
 print()
-print("All validations are done!")
+print("# Scenario 2")
+print(None == 0)
+
+x = 0
+y = 0
+print(x == y)
+
+print(True or False)
+print(False and True)
+print(not True)
+
+
+# Scenario 3
+# Explaining global in Python code
+print()
+print("# Scenario 3")
+var_a = 10
+
+# Function to read variable
+def read():
+    print("Before modifications - ", var_a)
+
+# Function to change the value of globally defined variable
+def mod_1():
+    global var_a
+    var_a = 20
+    print("mod_1() - After modifications - ", var_a)
+
+# Function to change the value of only local variable
+def mod_2():
+    var_a = 15
+    print("mod_2() - After modifications - ", var_a)
+
+# Calling functions defined
+read()
+mod_1()
+read()
+mod_2()
+read()
+
+# Scenario 4
+# Explaining non global in Python code
+print()
+print("# Scenario 4")
+def outer_nonlocal():
+    var_b = 5
+    def inner():
+        nonlocal var_b
+        var_b = 10
+    inner()
+    print("Output after using nonlocal - ", var_b)
+
+outer_nonlocal()
+
+def outer():
+    var_b = 5
+    def inner():
+        var_b = 10
+    inner()
+    print("Output without using nonlocal - ", var_b)
+
+outer()
